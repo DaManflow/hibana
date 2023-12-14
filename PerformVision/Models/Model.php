853f2590fb79dcad6d7238aaec3e34a890b0ed12
NathKaden <boulangeot.nathan@gmail.com>
2023-12-14 15:46:28 +0100

    Update

---
+++ PerformVision/Models/Model.php
@@ -0,0 +1,28 @@
+<?php
+
+class Model
+{
+    /**
+     * Attribut contenant l'instance PDO
+     */
+    private $bd;
+
+    /**
+     * Attribut statique qui contiendra l'unique instance de Model
+     */
+    private static $instance = null;
+
+    /**
+     * Constructeur : effectue la connexion à la base de données.
+     */
+    private function __construct()
+    {
+        include "/home/Web/Auth/credentials.php";
+        $this->bd = new PDO($dsn, $login, $mdp);
+        $this->bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->bd->query("SET nameS 'utf8'");
+    }
+
+
+
+}
