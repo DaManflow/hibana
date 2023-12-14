779f169157a5fa64a62e0fe789804b1417fbc85d
NathKaden <boulangeot.nathan@gmail.com>
2023-12-14 15:52:13 +0100

    Update

---
+++ PerformVision/Views/view_begin.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>PerformVision</title>
+		<link rel="stylesheet" href="Content/css/nobel.css"/>
+	</head>
+	<body>
---
+++ PerformVision/Views/view_home.php
@@ -0,0 +1,22 @@
+<?php require "view_begin.php"; ?>
+
+<div id="top" class="div1"></div>
+
+<div class="Framenav">
+
+    <a class="AccButton" href="#top" title="Retour à l'accueil">
+
+        <p class="Accueil">PerformVision Training & Consulting
+        </p>
+    </a>
+
+    <div class="navi">
+        <div><a href="#Formations" title="Formations" class="link">Formations</a></div>
+        <div><a href="#Conseils" title="Conseils" class="link">Conseils</a></div>
+        <div><a href="#Autres ▽" title="Autres" class="link mecontacter">Autres ▽</a></div>
+        <div><a href="#se connecter" title="Se connecter" class="link">Se connecter</a></div>
+    </div>
+
+</div>
+
+<?php require "view_end.php"; ?>
