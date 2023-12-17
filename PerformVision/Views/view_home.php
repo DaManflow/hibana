8960df1111c6ab09b60e2ca2f844a53db62b2a26
NathKaden <nathan.boulangeot@edu.univ-paris13.rf>
2023-12-17 17:49:00 +0100

    update

--- PerformVision/Views/view_home.php
+++ PerformVision/Views/view_home.php
@@ -2,19 +2,30 @@
 <?php
 require_once "view_begin.php";?>
 
-    <div class="Framenav">
+    <div class="framenav">
 
-    <a class="AccButton" href="?" title="Retour à l'accueil">
-        <p class="Accueil">PerformVision Training & Consulting
-        </p>
-    </a>
+        <a class="AccButton" href="?" title="Retour à l'accueil">
+            <p class="Accueil">PerformVision Training & Consulting
+            </p>
+        </a>
 
-    <div class="navi">
-        <div><a class="link" href="#Formations" title="Formations">Formations</a></div>
-        <div><a class="link" href="#Conseils" title="Conseils">Conseils</a></div>
-        <div><a class="naviautres" href="#Autres ▽" title="Autres">Autres ▽</a></div>
+        <div class="navi">
+            <div><a class="link" href="#Formations" title="Formations">Formations</a></div>
+            <div><a class="link" href="#Conseils" title="Conseils">Conseils</a></div>
 
-        <div><a class="naviconnexion" href="?controller=register&action=form_register_customer" title="Se connecter">Se connecter</a></div>
+            <div class="autres">
+            <button class="btn">Autres
+            </button>
+            <div class="menuautres">
+                <a class="link" href="un">jesaispas</a>
+                <a class="link" href="de">jesaispas</a>
+                <a class="link" href="tr">jesaispas</a>
+            </div></div>
+
+
+
+            <div><a class="naviconnexion" href="?controller=register&action=form_register_customer" title="Se connecter">Se connecter</a></div>
+        </div>
+
+
     </div>
-
-</div>
