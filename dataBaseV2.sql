929da4bb78d7c03ef237b232c6a84ff70fc9d9b0
Manon GLAUDE <manon.glaude@edu.univ-paris13.fr>
2023-12-18 16:01:49 +0000

    update dataBaseV2.sql

--- dataBaseV2.sql
+++ dataBaseV2.sql
@@ -6,12 +6,13 @@ CREATE TABLE utilisateur(
    prenom VARCHAR(255) NOT NULL,
    mail VARCHAR(255) NOT NULL,
    password VARCHAR(255) NOT NULL,
-   telephone INTEGER,
+   telephone INTEGER NOT NULL,
    PRIMARY KEY(id_utilisateur)
 );
 
 CREATE TABLE client(
    id_client INTEGER,
+   societe VARCHAR(255) NOT NULL,
    PRIMARY KEY(id_client),
    FOREIGN KEY(id_client) REFERENCES utilisateur(id_utilisateur)
 );
@@ -20,15 +21,103 @@ CREATE TABLE formateur(
    id_formateur SMALLINT,
    linkedin VARCHAR(255) NOT NULL,
    cv bytea NOT NULL,
-   date_signature date NOT NULL,
+   date_signature DATE NOT NULL,
    PRIMARY KEY(id_formateur),
    FOREIGN KEY(id_formateur) REFERENCES Utilisateur(id_utilisateur)
 );
 
 CREATE TABLE moderateur(
    id_moderateur SMALLINT,
-
    PRIMARY KEY(id_moderateur),
-   FOREIGN KEY(id_moderateur) REFERENCES Formateur(id_utilisateur)
+   FOREIGN KEY(id_moderateur) REFERENCES Formateur(id_formateur)
 );
 
+CREATE TABLE admin(
+   id_admin SMALLINT,
+   PRIMARY KEY(id_admin),
+   FOREIGN KEY(id_admin) REFERENCES formateur(id_formateur)
+);
+
+CREATE TABLE discussion(
+   id_discussion INTEGER,
+   id_client INTEGER NOT NULL,
+   id_formateur INTEGER NOT NULL,
+   PRIMARY KEY(id_discussion),
+   FOREIGN KEY(id_client) REFERENCES client(id_client),
+   FOREIGN KEY(id_formateur) REFERENCES formateur(id_formateur)
+);
+
+CREATE TABLE message(
+   id_message INTEGER,
+   id_discussion INTEGER,
+   texte VARCHAR(1500),
+   date_heure TIMESTAMP NOT NULL,
+   valideM BOOLEAN NOT NULL,
+   lu BOOLEAN NOT NULL,
+   PRIMARY KEY(id_message, id_discussion),
+   FOREIGN KEY(id_discussion) REFERENCES discussion(id_discussion)
+);
+
+CREATE TABLE activite(
+   id_activite SMALLINT,
+   nom_activite VARCHAR(100) NOT NULL,
+   image VARCHAR(400),
+   texte VARCHAR(255),
+   PRIMARY KEY(id_activite)
+);
+
+CREATE TABLE categorie(
+   idC SERIAL,
+   nomC VARCHAR(255) NOT NULL,
+   valideC BOOLEAN NOT NULL,
+   idC_mere SERIAL,
+   PRIMARY KEY(idC),
+   FOREIGN KEY(idC_mere) REFERENCES categorie(idC)
+);
+
+CREATE TABLE theme(
+   idT SERIAL,
+   nomT VARCHAR NOT NULL,
+   valideT BOOLEAN NOT NULL,
+   idC SERIAL,
+   PRIMARY KEY(idT),
+   FOREIGN KEY(idC) REFERENCES categorie(idC)
+);
+
+CREATE TABLE niveau(
+   idN SMALLINT,
+   libelle VARCHAR(255) NOT NULL,
+   PRIMARY KEY(idN)
+);
+
+CREATE TABLE public(
+   idP SERIAL,
+   libelleP VARCHAR(255) NOT NULL,
+   PRIMARY KEY(idP)
+);
+
+CREATE TABLE aExpertiseProfessionnelle(
+   idN SMALLINT,
+   idT SERIAL,
+   id_formateur SMALLINT,
+   dureeMExperience SMALLINT NOT NULL,
+   commentaire_expertise VARCHAR(255),
+   PRIMARY KEY(idN, idT, id_formateur),
+   FOREIGN KEY(idN) REFERENCES niveau(idN),
+   FOREIGN KEY(idT) REFERENCES theme(idT),
+   FOREIGN KEY(id_formateur) REFERENCES formateur(id_formateur)
+
+);
+
+CREATE TABLE aExperiencePeda(
+   id_formateur SMALLINT,
+   idT SERIAL,
+   idP SERIAL,
+   volumeHMoyenSession SMALLINT NOT NULL,
+   nbSessionEffectuee SMALLINT NOT NULL,
+   commentaire VARCHAR(255),
+   PRIMARY KEY(id_formateur, idT, idP),
+   FOREIGN KEY(idP) REFERENCES public(idP),
+   FOREIGN KEY(idT) REFERENCES theme(idT),
+   FOREIGN KEY(id_formateur) REFERENCES formateur(id_formateur)
+);
