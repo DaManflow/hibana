90973fe80f255210b402352b3c3ae638b86783bf
kenza-hatem <119787976+kenza-hatem@users.noreply.github.com>
2023-12-21 23:12:50 +0100

    Correction

--- PerformVision/Views/view_begin.php
+++ PerformVision/Views/view_begin.php
@@ -5,18 +5,14 @@
         PerformVision
     </title>
     <meta charset="utf-8"/>
-<<<<<<< HEAD
     <link rel="stylesheet" href="./Content/css/style.css"/>
     <link rel="icon" href="./Content/images/logo.png" />
-=======
-<<<<<<< HEAD
+
     <link rel="stylesheet" href="./Content/css/style.css"/>
     <link rel="pp" href="" />
-=======
     <link rel="stylesheet" href="../Content/css/style.css"/>
     <link rel="pp" href="../Content/images/logo.png" />
->>>>>>> e066e54daddac62adbffcfc86cd72baf6660fb64
->>>>>>> 05cf055715e0c4da439cfef9f5a4aba38d524911
+
     <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet" />
 </head>
 <body>
