7cca873b4bde2cc371ef9097666d65e5c664a8ff
NathKaden <boulangeot.nathan@gmail.com>
2023-12-21 12:32:18 +0100

    Update

--- PerformVision/Views/view_begin.php
+++ PerformVision/Views/view_begin.php
@@ -2,11 +2,11 @@
 <html lang="fr">
 <head>
     <title>
-        Titre
+        PerformVision
     </title>
     <meta charset="utf-8"/>
     <link rel="stylesheet" href="../Content/css/style.css"/>
-    <link rel="pp" href="" />
+    <link rel="pp" href="../Content/images/logo.png" />
     <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet" />
 </head>
 <body>
