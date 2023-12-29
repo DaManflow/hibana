48441627df435f4b1894d98a27e5240c375dff07
guillauby djibryl <djibryl.guillauby@gmail.com>
2023-12-29 19:23:24 +0100

    .htaccess et view_begin

--- PerformVision/Views/view_begin.php
+++ PerformVision/Views/view_begin.php
@@ -5,8 +5,8 @@
         PerformVision
     </title>
     <meta charset="utf-8"/>
-    <link rel="stylesheet" href="/hibana-main/PerformVision/Content/css/style.css"/>
-    <link rel="icon" href="./Content/images/logo.png" />
+    <link rel="stylesheet" type="text/css" href="/hibana-main/PerformVision/Content/css/style.css"/>
+    <link rel="icon" href="/hibana-main/PerformVision/Content/images/logo.png" />
 
     <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet" />
 </head>
