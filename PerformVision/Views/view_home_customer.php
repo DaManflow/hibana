145ef2fd6ed281f743769e7ed51b3eeb9b2dab1a
kenza-hatem <119787976+kenza-hatem@users.noreply.github.com>
2024-01-01 02:46:17 +0100

    Page d'accueil responsive

--- PerformVision/Views/view_home_customer.php
+++ PerformVision/Views/view_home_customer.php
@@ -10,40 +10,80 @@ if (isset($_SESSION['idutilisateur']) && $_SESSION['role'] == "formateur") {
 
 ?>
 
-<div class="framenavbg"></div>
-<div class="framenav">
+<div class="debut">
+    <div class="partie-gauche">PerformVision Training & Consulting</div>
+    <div class="partie-droite">
+        <ul class="ul1">
+        <li class="formations"><a href="/hibana-main/PerformVision/?controller=former_list&action=former_pagination">Formations</a></li>
+        <li class="conseils">Conseils</li>
+        <li class="autres">Autres        
+        <ul class="dropdown">
+            <li>Activité 1</li>
+            <li>Activité 2</li>
+            <li>Activité 3</li>
+        </ul>
+</li>
 
-    <a class="AccButton" href="/hibana-main/PerformVision/?controller=home_customer&action=home_customer" title="Retour à l'accueil">
-        <p class="Accueil">PerformVision Training & Consulting</p>
-    </a>
+        <li class="connect"><a href="/hibana-main/PerformVision/?controller=profil_customer&action=profil_customer"><button><i class="fa-regular fa-circle-user"></i><span class="aut">Mon Profil</span></button></a></li>
+</ul>
+    </div>
+</div>
 
-    <div class="navi">
-        <div><a class="link" href="/hibana-main/PerformVision/?controller=former_list&action=former_pagination" title="Formateurs">Formateurs</a></div>
-        <div><a class="link" href="#Conseils" title="Conseils">Conseils</a></div>
-
-        <div class="autres">
-            <button class="btn">Autres ▽</button>
-            <div class="menuautres">
-                <a class="link" href="un">Activité 1</a>
-                <a class="link" href="de">Activité 2</a>
-                <a class="link" href="tr">Activité 3</a>
-            </div>
+<div class="titles">
+    <h2 class="h2">Bienvenue chez</h2>
+    <h1 class="h1">PerformVision Formations</h1>
+</br>
+<a href="/hibana-main/PerformVision/?controller=&action=formateur"><button class="form-title">Découvrir les Formations</button></a>
+</div>
+<div class="reste">
+<div class="centre">
+    <div class="conteneur1">
+    <div class="img1">
+        <img src="/hibana-main/PerformVision/Content/images/ProgrammeurQuiFaitDuC.jpg"/>
+    </div>
+    </div>
+        <div class="conteneur2">
+            <div class="img2">
+                <img src="/hibana-main/PerformVision/Content/images/HommeQuiEstHeureuxDeTravailler.jpg"/>
         </div>
-        <div><a class="naviconnexion" href="/hibana-main/PerformVision/?controller=profil_customer&action=profil_customer" title="Mon profil">Mon profil</a></div>
-    </div>
+        <div class="text">
+            Que vous soyez en autodidacte, salarié(e) ou en reconvresion professionnelle,<span> PerformVision</span> vous propose<span> la formation qu'il vous faut</span>
+        </div>
+        </div>
+    <div class="conteneur3">
+        <div class="img3">
+            <img src="/hibana-main/PerformVision/Content/images/HommeQuiEstHeureuxDeTravailler.jpg"/>
+        </div>
+    <div class="text2">
+        Devenez <span>développeur informatique</span><br/>Suivez des cours auprès de formateurs <span>expérimentés</span>, apprenez le python, C, C++, Java, SQL et bien d'autres !
+    </div> 
 </div>
-
-<div class="color"></div>
-<div class="div1"></div>
-<div class="divmain">
-    <h2>Bienvenue chez</h2>
-    <h1>PerformVision Training & Consulting</h1>
-    <a class="btndecouvrir" href="#decouvrir" title="Aller à la page formateurs">Découvrir les formateurs</a>
-    <div class="divwhite">
-        <div class="titrebg"><p>Découvrez les formations <span>qui vous correspondent</span></p></div>
-        <div class="divimg"></div>
-    </div>
 </div>
-
-<div class="footer"></div>
+</div>
+<footer>
+    <div class="partie1">
+        <div class="partie1-gauche">
+            <h4>Nous contacter</h4>
+            <a href="#">06 22 22 22 22</a>
+            <a href="#">hibana.sae103@gmail.com</a>
+        </div>
+        <!--
+        <div class="partie1-droite">
+            <h4>Nos réseaux</h4>
+            <ul>
+            <li><a href="#">1</a></li>
+            <li><a href="#">2</a></li>
+            <li><a href="#">3</a></li>
+            <li><a href="#">4</a></li>
+            </ul>
+        </div>
+    -->
+    </div>
+    <div class="partie2">
+        <h1>PerformVision Formations</h1>
+        <p class="droits">
+            ©2023 Tout droits résevés
+        </p>
+    </div>
+</footer>
 <?php require_once "view_end.php"; ?>
