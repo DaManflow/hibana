cc97404b903bd92ea81ea38eaee8f5e735986440
NathKaden <nathan.boulangeot@edu.univ-paris13.rf>
2024-01-11 17:35:31 +0100

    Update

--- PerformVision/Views/view_form_login.php
+++ PerformVision/Views/view_form_login.php
@@ -3,11 +3,11 @@
 <body class="bodyform">
 
 <h1> Se connecter </h1>
-<form action="?controller=login&action=connectUser" method="POST">
 
+<form action="?controller=login&action=connectUser" method="POST">
     <p> <label> <input required="" type="text" name="email" placeholder="Email"/> </label> </p>
     <p> <label> <input required="" type="password" name="password" placeholder="Mot De Passe"/> </label> </p>
-    <p> <label> <input class="connect" required="" type="submit" name="submit" value="Se connecter"> </label> </p>
+    <p> <label> <input required="" type="submit" name="submit" value="Se connecter"> </label> </p>
 </form>
 
 Vous n'êtes pas encore inscrit ?  <a class="lienbtn" href="?controller=choice&action=register_choice">Inscrivez-Vous</a>
--- PerformVision/Views/view_register_choice.php
+++ PerformVision/Views/view_register_choice.php
@@ -1,13 +1,17 @@
 <?php   require "view_begin.php";?>
 
 
+<body class="bodyform">
 
-<p> <a href="?controller=register_former&action=form_register_former"> S'inscrire en tant que formateur</a> </p>
-<p> <a href="?controller=register_customer&action=form_register_customer"> S'inscrire en tant que client</a> </p>
-
-
-
-<?php   
+<h1>S'inscrire</h1>
 
 
+<div class="divchoice">
+<p>Vous voulez proposer vos formations?</p>
+<p> <a class="lienbtn" href="?controller=register_former&action=form_register_former">S'inscrire en tant que formateur</a> </p>
+<p>Vous cherchez une formation?</p>
+<p> <a class="lienbtn" href="?controller=register_customer&action=form_register_customer">S'inscrire en tant que client</a> </p>
+</div>
+
+<?php
 require "view_end.php"; ?>
