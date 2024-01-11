461ac840254d8d6f288f1f6dcdd4bb5b130839a5
NathKaden <nathan.boulangeot@edu.univ-paris13.rf>
2024-01-11 15:34:16 +0100

    Optimisation view accueil

--- PerformVision/Views/view_form_login.php
+++ PerformVision/Views/view_form_login.php
@@ -7,9 +7,6 @@
     <p> <label> <input required="" type="text" name="email" placeholder="Email"/> </label> </p>
     <p> <label> <input required="" type="password" name="password" placeholder="Mot De Passe"/> </label> </p>
     <p> <label> <input required="" type="submit" name="submit" value="Se connecter"> </label> </p>
-    
-
-
 </form>
 
 Vous n'êtes pas encore inscrit ? <a href="?controller=choice&action=register_choice">Inscrivez-Vous</a>
