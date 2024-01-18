22951eab1219c4aa445b2a6b691bfe563e083926
guillauby djibryl <djibryl.guillauby@gmail.com>
2024-01-18 20:58:24 +0100

    update

--- PerformVision/Views/view_themes_non_valides.php
+++ PerformVision/Views/view_themes_non_valides.php
@@ -7,7 +7,8 @@
 <?php 
 echo '<div class="themes"><table>' ; 
 foreach ($themesNonValides as $theme) {
-    echo '<tr><p><td>' . $theme['nomt'] . '</td><td> <button onclick="devalider(' . $theme['idt'] . ')"> Valider</button>      <button> Refuser </button></td></p></tr>';
+    echo '<tr><td>' . $theme['nomt'] . '</td><td><a href="?controller=list_themes&action=valider_theme&idt=' . $theme['idt'] . '"><button>Valider</button></a><a href="?controller=list_themes&action=supprimer_theme&idt=' . $theme['idt'] . '"><button>Refuser</button></a></td></tr>';
+
 } 
 echo '</table></div>' ; ?>
 <?php require "view_end.php" ; ?>
